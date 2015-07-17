c79df1933140b5fd24134e8b44277627f99061e7
Chandan Jyoti Sharma <chandan.jyoti.sharma@intel.com>
2015-07-16 18:49:09 -0700

    Initial working. Needs to be tapped quickly to see bird. MidpointY hardcoded

--- core/src/com/kilobolt/screens/GameScreen.java
+++ core/src/com/kilobolt/screens/GameScreen.java
@@ -6,57 +6,69 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.com.kilobolt.gameworld.GameRenderer;
 import com.com.kilobolt.gameworld.GameWorld;
+import com.com.kilobolt.inputhandler.inputHandler;
 
 /**
  * Created by chandanj on 7/11/2015.
  */
 public class GameScreen implements Screen {
-    GameRenderer gameRenderer;
-    GameWorld gameWorld;
-    public GameScreen()
-    {
-        Gdx.app.log("GameScreen", "Attached");
-        Gdx.app.log("GameScreen", "Attached");
-        gameWorld= new GameWorld();
-        gameRenderer =  new GameRenderer(gameWorld);
-    }
+    //all helper classes go here
+    private GameWorld world;
+    private GameRenderer renderer;
+    private float runTime;
 
-    @Override
-    public void show() {
-        Gdx.app.log("GameScreen", "show called");
+    // This is the constructor, not the class declaration
+    public GameScreen() {
+
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+        float gameWidth = 136;
+        float gameHeight = screenHeight / (screenWidth / gameWidth);
+
+        //int midPointY = (int) (gameHeight / 2);
+        int midPointY = -8;
+        world = new GameWorld(midPointY);
+        renderer = new GameRenderer(world, (int) gameHeight, midPointY);
+
+        Gdx.input.setInputProcessor(new inputHandler(world.getBird()));
+
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(10 / 255.0f, 15 / 255.0f, 230 / 255.0f, 1f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //Gdx.app.log("GameScreen FPS",1/delta+ "");
-        gameWorld.update(delta);
-        gameRenderer.render();
+        runTime += delta;
+        world.update(delta);
+        renderer.render(runTime);
     }
 
     @Override
     public void resize(int width, int height) {
-        Gdx.app.log("GameScreen", "resizing");
+        System.out.println("GameScreen - resizing");
     }
 
     @Override
-    public void pause() {
-        Gdx.app.log("GameScreen", "pause called");
-    }
-
-    @Override
-    public void resume() {
-        Gdx.app.log("GameScreen", "resume called");
+    public void show() {
+        System.out.println("GameScreen - show called");
     }
 
     @Override
     public void hide() {
+        System.out.println("GameScreen - hide called");
+    }
 
+    @Override
+    public void pause() {
+        System.out.println("GameScreen - pause called");
+    }
+
+    @Override
+    public void resume() {
+        System.out.println("GameScreen - resume called");
     }
 
     @Override
     public void dispose() {
-
+        // Leave blank
     }
+
 }
